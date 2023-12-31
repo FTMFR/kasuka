6b8b9ca1dc74780eb20e995ca479361f0b16dbd0
FTMFR <fatemefarajzade018@gmail.com>
2023-12-31 15:15:57 +0330

    add logo component

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./logo.scss";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./assets/logo.png" alt="logo" />
+      <p>KASUKA</p>
+    </div>
+  );
+};
+
+export default Logo;
