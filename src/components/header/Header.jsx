fbefeca73ae009c64e6c5c5bb808483546686325
FTMFR <fatemefarajzade018@gmail.com>
2023-12-31 15:16:36 +0330

    clean my workship

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../logo/Logo";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="startBtn">شروع</div>
+      <div className="titles">
+        <ul>
+          <li>خانه</li>
+          <li>درباره ما</li>
+          <li>خدمات</li>
+          <li>نمونه کار</li>
+          <li>تیم</li>
+          <li>لیست کشویی</li>
+          <li>تماس با ما</li>
+        </ul>
+      </div>
+<Logo />
+    </div>
+  );
+};
+
+export default Header;
