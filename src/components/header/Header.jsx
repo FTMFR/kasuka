c98d18101fa037e9a65f5e1bdce00f1f505385c6
FTMFR <fatemefarajzade018@gmail.com>
2023-12-31 22:35:40 +0330

    fix a bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,19 +5,21 @@ import "./header.scss";
 const Header = () => {
   return (
     <div className="container">
-      <div className="startBtn">شروع</div>
-      <div className="titles">
-        <ul>
-          <li>خانه</li>
-          <li>درباره ما</li>
-          <li>خدمات</li>
-          <li>نمونه کار</li>
-          <li>تیم</li>
-          <li>لیست کشویی</li>
-          <li>تماس با ما</li>
-        </ul>
+      <div className="contain">
+        <div className="startBtn">شروع</div>
+        <div className="titles">
+          <ul>
+            <li>خانه</li>
+            <li>درباره ما</li>
+            <li>خدمات</li>
+            <li>نمونه کار</li>
+            <li>تیم</li>
+            <li>لیست کشویی</li>
+            <li>تماس با ما</li>
+          </ul>
+        </div>
+        <Logo />
       </div>
-      <Logo />
     </div>
   );
 };
