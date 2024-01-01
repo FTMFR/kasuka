28eb7c21af354526ada5512d4418c7001709afd1
FTMFR <fatemefarajzade018@gmail.com>
2024-01-01 14:58:19 +0330

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import SubNav from './components/header/subNav/SubNav';
+import Body from './components/body/Body';
 import './App.css';
 
 function App() {
   return (
+    <>
     <SubNav />
+    <Body />
+    </>
   );
 }
 
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./body.scss";
+
+const Body = () => {
+  return <div>
+    <div className="title"></div>
+    <div className="text"></div>
+    <img src="./assets/about.jpg" alt="about company" />
+  </div>;
+};
+
+export default Body;
